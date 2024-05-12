a6a856863a39ee8b61953cef59372c9e7d8cfb40
Zightch <Zightch@163.com>
2024-05-12 15:43:19 +0800

    .

--- CCP/CCP.h
+++ CCP/CCP.h
@@ -63,7 +63,7 @@ private:
     // NA数据包不需要走发送缓存和发送窗口, 直接发送
 
     unsigned short wndSize = 256; // 窗口大小
-    unsigned short dataBlockSize = 5; // 可靠传输时数据块大小
+    unsigned short dataBlockSize = 13; // 可靠传输时数据块大小, 测试用13, 生产环境默认1013
     QTimer hbt; // 心跳包定时器
     unsigned short hbtTime = 15000; // 心跳时间
     QHostAddress IP; // 远程主机IP
